6bd38d792e97f78e6defa4b92848b81f0bd83446
Kristian Mitra <kristian.m@anino.co>
2017-10-07 14:23:04 +0800

    Created base StudentsController

---
+++ ContosoCore/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContosoCore.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentController(SchoolContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            return View(await this._context.Students.ToListAsync());
+        }
+    }
+}
