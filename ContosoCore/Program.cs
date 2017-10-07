e62f463f0802c64682135e10aaf46b8000388bf6
Kristian Mitra <kristian.m@anino.co>
2017-10-07 14:24:18 +0800

    Move initializer to Main()

--- ContosoCore/Program.cs
+++ ContosoCore/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCore.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ContosoCore
 {
@@ -18,6 +21,21 @@ namespace ContosoCore
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(new EventId(), ex, "An error occurred while seeding the database.");
+                }
+            }
 
             host.Run();
         }
